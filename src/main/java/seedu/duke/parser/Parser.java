b3144ae8abeb44bbd7fc333d8bc2d57b29668257
tobiasceg <e0415553@u.nus.edu>
2020-10-03 20:59:42 +0800

    Added parser,textui and messages

--- src/main/java/seedu/duke/parser/Parser.java
+++ src/main/java/seedu/duke/parser/Parser.java
@@ -1,4 +1,28 @@
 package seedu.duke.parser;
 
 public class Parser {
+    public static String parseCommand(String userInput){
+        switch (userInput){
+        case "add":
+            return addCommand(userInput);
+
+        case "delete":
+            return deleteCommand(userInput);
+
+        case "list":
+            return listCommand(userInput);
+
+        case "edit":
+            return editCommand(userInput);
+
+        case "done":
+            return doneCommand(userInput);
+
+        case "bye":
+            return byeCommand(userInput);
+
+        default:
+            return HelpCommand();
+        }
+    }
 }
--- src/main/java/seedu/duke/util/Message.java
+++ src/main/java/seedu/duke/util/Message.java
@@ -1,4 +1,6 @@
 package seedu.duke.util;
 
 public class Message {
+    public static final String MESSAGE_GOODBYE = "Good bye!";
+    public static final String MESSAGE_WELCOME = "Welcome to RAVI";
 }
